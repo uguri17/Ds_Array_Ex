e95ba017906b80d203b73b15d055065a87a9d2df
RND-HW3 <RND-HW3@RND-HW3>
2025-03-17 13:01:00 +0900

    초기 커밋.

---
+++ Ds_Array_Ex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ds_Array_Ex
+{
+    // Array of DataStructure
+    class ArrayExample 
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = { 10, 20, 30, 40, 50 };
+
+			
+			Console.WriteLine("for loop를 이용한 array[] 출력: ");
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				Console.WriteLine($"Index {i}: {numbers[i]}");
+			}
+
+
+			Console.WriteLine("\nforeach loop를 이용한 array[] 출력: ");
+			foreach (int num in numbers)
+			{
+				Console.WriteLine(num);
+			}
+		}
+	}
+}
